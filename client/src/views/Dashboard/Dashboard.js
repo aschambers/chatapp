a45c79687c593bb2711f2bc7856d0e89454397e1
aschambers <aschambers4291@gmail.com>
2019-08-04 13:00:18 -0700

    add code for onClickOutside for testing, but later make full screen so won't be needed but good for an example to see later when wanting this ability

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../redux';
+import ReactTooltip from 'react-tooltip';
 import Chatroom from '../../components/Chatroom/Chatroom';
 import './Dashboard.css';
 import chatot from '../../assets/images/chatot.png';
@@ -12,10 +13,17 @@ const Dashboard = (props) => {
   const [imageUrl, setImageUrl] = useState('');
   const [active, setActive] = useState(false);
   const [chatroom, setChatroom] = useState('');
+  const [hover, setHover] = useState('');
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const ref = useRef();
+  const settingsRef = useRef("settings");
+  useOnClickOutside(ref, () => setModalOpen(false));
 
   useEffect(() => {
     if(!props.user) {
       props.currentUser();
+      window.addEventListener('keydown', detectEscape);
     } else if(props.user) {
       const { username, imageUrl, active } = props.user;
       setUsername(username);
@@ -24,18 +32,28 @@ const Dashboard = (props) => {
     }
   }, [props]);
 
+  const detectEscape = (event) => {
+    if (event.keyCode === 27) {
+      setModalOpen(false);
+    }
+  }
+
   return (
     <div className="chatroom">
       <div className="sidebar">
-        <div className="sidebar-container">
-          <span className="sidebar-select"></span>
+        <div className="sidebar-container" data-tip="home" data-place="right" onPointerOver={() => { setHover("") }}>
+          {hover === "" && chatroom !== "" ? <span className="sidebar-hover"></span> : null}
+          {chatroom === "" ? <span className="sidebar-select"></span> : null}
           <img className="sidebar-logo" src={chatot} alt="chatter-icon-logo" onClick={() => { setChatroom("") }} />
         </div>
         <div className="sidebar-border" />
-        <div className="sidebar-container">
+        <div className="sidebar-container" data-tip={username} data-place="right" onPointerOver={() => { setHover(username) }}>
+          {hover === username && chatroom !== username ? <span className="sidebar-hover"></span> : null}
+          {chatroom === username ? <span className="sidebar-select"></span> : null}
           <img className="sidebar-logo" src={chatot} alt="chatter-icon" onClick={() => { setChatroom(username) }} />
         </div>
       </div>
+      <ReactTooltip delayShow={200} />
       <div className="sidebarleft">
         <div className="sidebarleft-container">
           <input placeholder="Find or start a conversation"></input>
@@ -56,7 +74,7 @@ const Dashboard = (props) => {
           </div>
           {active ? <div className="userinfo-online"></div> : null}
           <span style={{ color: 'white' }}>{username}</span>
-          <img className="settings-image" src={settings} alt="settings-icon" />
+          <img className="settings-image" src={settings} alt="settings-icon" ref={settingsRef} onClick={() => { setModalOpen(!isModalOpen); }} />
         </div>
       </div>
       {chatroom === username ?
@@ -78,8 +96,34 @@ const Dashboard = (props) => {
           </div>
         </div>
       }
+      {isModalOpen ?
+        <div className="modal-settings" ref={ref}>
+          Hey, I'm a modal. Click anywhere outside of me to close.
+        </div>
+      : null}
     </div>
   );
+}
+
+const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = event => {
+      // Do nothing if clicking ref's element, descendent elements or settings icon
+      if (!ref.current || ref.current.contains(event.target) || event.target.className === "settings-image") {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
 }
 
 function mapStateToProps({ usersReducer }) {
