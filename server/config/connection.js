19b12929f991c6ccba3f101e0b8af25758630ef9
aschambers <aschambers4291@gmail.com>
2019-09-08 18:48:09 -0700

    add amazon database connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize("REMOVED", "REMOVED", "REMOVED", {
   dialect: "postgres",
-  host: "127.0.0.1",
+  host: "REMOVED1.crmkekcbvhcf.us-west-1.rds.amazonaws.com",
+  // host: "127.0.0.1",
   port: 5432,
   pool: {
     max: 5,
@@ -11,7 +12,8 @@ const sequelize = new Sequelize("REMOVED", "REMOVED", "REMOVED", {
     aquire: 60000
   },
   dialectOptions: {
-    socketPath: "127.0.0.1",
+    socketPath: "REMOVED1.crmkekcbvhcf.us-west-1.rds.amazonaws.com",
+    // socketPath: "127.0.0.1"
   }
 });
 
