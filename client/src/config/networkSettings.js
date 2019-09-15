c631bdba4f10d944e3b729f21fc92fcd4a040fca
aschambers <aschambers4291@gmail.com>
2019-09-14 21:14:19 -0700

    fix a style issue with messages on chatrooms

--- client/src/config/networkSettings.js
+++ client/src/config/networkSettings.js
@@ -1,4 +1,4 @@
-// export const ROOT_URL = 'http://localhost:5000';
-export const ROOT_URL = 'https://chattersanctum.com'; // Server
+export const ROOT_URL = 'http://localhost:5000';
+// export const ROOT_URL = 'https://chattersanctum.com'; // Server
 export const HASH = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.vfQWFfZPQNJ-gz3qYqHtb3qfuMJsQV02RPckLoQS3So';
 export const JWT_TOKEN = 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.6p1XVDcBXV904-cP0Dyd6GXsDJ0PMjfnDL1e2mO3N3E';
