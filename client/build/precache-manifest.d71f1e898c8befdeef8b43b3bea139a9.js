a5ce970089f508a7a594357f215770dfdc6180e4
aschambers <aschambers4291@gmail.com>
2019-09-15 09:45:05 -0700

    fix a few slight issues found

--- client/build/precache-manifest.d71f1e898c8befdeef8b43b3bea139a9.js
+++ client/build/precache-manifest.d71f1e898c8befdeef8b43b3bea139a9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b5eaacbd23bc870ac0395b8d89a97f7",
+    "revision": "35538b50cb22323b385c52bfd494d476",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.39017165.chunk.css"
   },
   {
-    "revision": "da6c354e0d681c590dae",
-    "url": "/static/css/4.d9d960ab.chunk.css"
+    "revision": "27df0bce7cde8349a2a9",
+    "url": "/static/css/4.50cb91e7.chunk.css"
   },
   {
     "revision": "52828fe5859eabe7c617",
@@ -32,8 +32,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.9b435719.chunk.js"
   },
   {
-    "revision": "da6c354e0d681c590dae",
-    "url": "/static/js/4.30f1594a.chunk.js"
+    "revision": "27df0bce7cde8349a2a9",
+    "url": "/static/js/4.f1579cb9.chunk.js"
   },
   {
     "revision": "52828fe5859eabe7c617",
@@ -48,8 +48,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/main.3c8b7ac1.chunk.js"
   },
   {
-    "revision": "e87ee9cab83ee12e39cc",
-    "url": "/static/js/runtime~main.781d7ed1.js"
+    "revision": "bc7abf324d407e59d45f",
+    "url": "/static/js/runtime~main.ebdc4626.js"
   },
   {
     "revision": "25cd38a8b03ff96775c2e00d31052010",
