d1ac1492421594e13762feef6d225586cf500548
aschambers <aschambers4291@gmail.com>
2019-09-18 23:49:21 -0700

    testing

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -41,7 +41,7 @@ const Signup = (props) => {
       active: false,
       type: 'user'
     }
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRegex = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!emailRegex.test(String(email).toLowerCase())) {
       toast.dismiss();
       toast.error('Email address must be a valid!', { position: 'bottom-center' });
