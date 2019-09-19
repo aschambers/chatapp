dbde32eab1a47851c15d1a713a0da318b171d2fd
aschambers <aschambers4291@gmail.com>
2019-09-19 00:17:17 -0700

    update build with better style  style for escape button x and menu for server settings

---
+++ client/build/precache-manifest.1e4ce10215493ae87924cf39905fbf7e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64aa9b9ea364bb9e5005d34f8f5f487f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70d8d1a4cfe00a3e577",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "7c60001e7ca8fc664590",
+    "url": "/static/css/4.1e2468a2.chunk.css"
+  },
+  {
+    "revision": "7ff737611d64130c7dda",
+    "url": "/static/css/5.a2490990.chunk.css"
+  },
+  {
+    "revision": "709e818cd334b1f368fe",
+    "url": "/static/css/6.0c3fa314.chunk.css"
+  },
+  {
+    "revision": "c6c4404d7e089bb4e94e",
+    "url": "/static/css/main.99802c2e.chunk.css"
+  },
+  {
+    "revision": "a70d8d1a4cfe00a3e577",
+    "url": "/static/js/2.a7f1c608.chunk.js"
+  },
+  {
+    "revision": "2cf46b888eb2c9758fe1",
+    "url": "/static/js/3.3cef93e9.chunk.js"
+  },
+  {
+    "revision": "7c60001e7ca8fc664590",
+    "url": "/static/js/4.7df3d3b2.chunk.js"
+  },
+  {
+    "revision": "7ff737611d64130c7dda",
+    "url": "/static/js/5.7324b41f.chunk.js"
+  },
+  {
+    "revision": "709e818cd334b1f368fe",
+    "url": "/static/js/6.bae72a54.chunk.js"
+  },
+  {
+    "revision": "c6c4404d7e089bb4e94e",
+    "url": "/static/js/main.3935f197.chunk.js"
+  },
+  {
+    "revision": "d918e0f784a6f67348ad",
+    "url": "/static/js/runtime~main.d74aeec2.js"
+  },
+  {
+    "revision": "25cd38a8b03ff96775c2e00d31052010",
+    "url": "/static/media/Antonio-Regular.25cd38a8.ttf"
+  },
+  {
+    "revision": "1e99b0442583593743adb5d91d7c0669",
+    "url": "/static/media/BebasNeue Bold.1e99b044.ttf"
+  },
+  {
+    "revision": "9042a37caa7fb0389371de54dd886b38",
+    "url": "/static/media/BebasNeue Regular.9042a37c.ttf"
+  },
+  {
+    "revision": "7eb68b80d0016066ae1bda295c223dc3",
+    "url": "/static/media/chatterapp.7eb68b80.png"
+  },
+  {
+    "revision": "8fc4fc5d06ac8b81d857dc2f388bbd29",
+    "url": "/static/media/europe.8fc4fc5d.png"
+  },
+  {
+    "revision": "16a4158a79c0aefee6fcab72f561d918",
+    "url": "/static/media/numbersign.16a4158a.png"
+  },
+  {
+    "revision": "70f288ec4ff7d64e87eb727dbc25f9b2",
+    "url": "/static/media/owner.70f288ec.png"
+  }
+]);
