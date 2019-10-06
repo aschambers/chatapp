8fe8a3d5ba50e93cf4a78e8f340377a9313adc42
aschambers <aschambers4291@gmail.com>
2019-10-05 21:56:44 -0700

    temporarily comment out auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,24 +1,25 @@
-const keys = require('../config/keys');
-const jwt = require('jsonwebtoken');
+// const keys = require('../config/keys');
+// const jwt = require('jsonwebtoken');
 
 module.exports = async(req, res, next) => {
-  try {
-    const type = req.headers.authorization.split(" ")[0];
-    if (type !== 'bearer') {
-      return res.status(401).json({
-        message: 'Auth failed'
-      });
-    }
+  next();
+  // try {
+  //   const type = req.headers.authorization.split(" ")[0];
+  //   if (type !== 'bearer') {
+  //     return res.status(401).json({
+  //       message: 'Auth failed'
+  //     });
+  //   }
 
-    const token = req.headers.authorization.split(" ")[1];
-    // verify and return decoded value
-    const decoded = await jwt.verify(token, keys.secret);
-    req.decoded = decoded;
-    //successfully authenticated user
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      message: 'Auth failed'
-    });
-  }
+  //   const token = req.headers.authorization.split(" ")[1];
+  //   // verify and return decoded value
+  //   const decoded = await jwt.verify(token, keys.secret);
+  //   req.decoded = decoded;
+  //   //successfully authenticated user
+  //   next();
+  // } catch (error) {
+  //   return res.status(401).json({
+  //     message: 'Auth failed'
+  //   });
+  // }
 }
