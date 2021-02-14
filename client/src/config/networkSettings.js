e22473354aeb0b4e2d5199aa2bf9ff9be666cfeb
Allison Schambers <aschambers4291@gmail.com>
2021-02-14 14:51:20 -0800

    Update build to point to heroku and add start to package.json file for heroku deployment.

--- client/src/config/networkSettings.js
+++ client/src/config/networkSettings.js
@@ -1,2 +1,3 @@
 // export const ROOT_URL = 'http://localhost:5000';
-export const ROOT_URL = 'https://chattersanctum.com';
+// export const ROOT_URL = 'https://chattersanctum.com';
+export const ROOT_URL = 'https://chattersanctum.herokuapp.com';
