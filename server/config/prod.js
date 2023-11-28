7a190d12cc2c1a37a2eaf54111036594c23f9592
Allison Schambers <aschambers4291@gmail.com>
2023-11-27 21:40:18 -0800

    remove sensitive keys from repo

---
+++ server/config/prod.js
@@ -0,0 +1,15 @@
+module.exports = {
+  secret: '',
+  cloudinary_name: '',
+  cloudinary_key: '',
+  cloudinary_secret: '',
+  sendgrid_key: '',
+  email_link: '',
+  server_url: '',
+  connection: '',
+  database_name: '',
+  database_username: '',
+  database_password: '',
+  ssl: true,
+  sslfactory: ''
+};
