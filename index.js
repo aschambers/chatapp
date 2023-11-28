d8ad3989b72e50e890db199290cf555fd1fef32f
Allison Schambers <aschambers4291@gmail.com>
2023-11-27 20:56:53 -0800

    Update server files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const busboyBodyParser = require('busboy-body-parser');
+
 // express setup
 const app = express();
 const auth = require('./server/middleware/auth');
@@ -9,15 +10,19 @@ app.use(helmet());
 app.use(auth);
 app.use(busboyBodyParser({ limit: '50mb', multi: true }));
 app.use(express.json());
+
 // cors
 const cors = require('cors');
-app.use(cors({ credentials: true, origin: true }));
+const whitelist = ['https://chattersanctum.com'];
+const corsOptions = {
+  origin: whitelist && whitelist.indexOf(origin) > -1,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+  credentials: true
+}
+app.use(cors(corsOptions));
 app.options('*', cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 // routes
 require('./server/routes/userRoutes')(app);
 require('./server/routes/messageRoutes')(app);
@@ -26,15 +31,18 @@ require('./server/routes/categoryRoutes')(app);
 require('./server/routes/serverRoutes')(app);
 require('./server/routes/inviteRoutes')(app);
 require('./server/routes/friendRoutes')(app);
+
 // client setup
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+
 // listen for api requests
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, function() {
   console.log('Server is listening on port: ' + PORT);
 });
+
 // chatroom
 require('./server/chatroom/chatroom')(server);
