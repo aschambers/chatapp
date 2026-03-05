793dc39f37e4f5f28582320fb711e0cd3d722f1b
aschambers <allisonschambers20@gmail.com>
2026-03-05 12:05:03 -0800

    fix: issues with signup after new changes, and switch to resend

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,19 +1,23 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { PrivateRoute } from './components/PrivateRoute/PrivateRoute.js';
 import Home from './views/Home/Home';
 import Loading from './components/Loading/Loading';
 import { BrowserRouter } from 'react-router-dom';
 
+const Dashboard = lazy(() => import("./views/Dashboard/Dashboard"));
+const Verification = lazy(() => import("./views/Verification/Verification"));
+const ResetPassword = lazy(() => import("./views/ResetPassword/ResetPassword"));
+
 const Main = () => (
   <Suspense fallback={<Loading />}>
     <BrowserRouter>
       <Routes>
         <Route path='/Dashboard' element={<PrivateRoute/>}>
-          <Route path='/Dashboard' lazy={() => import("./views/Dashboard/Dashboard")}/>
+          <Route path='/Dashboard' element={<Dashboard />}/>
         </Route>
-        <Route path="/Verification" element={() => import("./views/Verification/Verification")} />
-        <Route path="/ResetPassword" element={() => import("./views/ResetPassword/ResetPassword")} />
+        <Route path="/Verification" element={<Verification />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
         <Route path="/Login" element={<Home />} />
         <Route path="/Signup" element={<Home />} />
         <Route path="/" element={<Home />} />
